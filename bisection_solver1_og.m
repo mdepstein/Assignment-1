30b4833545d2a0362ef30c4da4a0bf82e0ac5d92
Kai-Xing Yue <79027796+Fantasticalkiwi@users.noreply.github.com>
2026-09-08 20:29:19 -0400

    update bisection

--- bisection_solver1_og.m
+++ bisection_solver1_og.m
@@ -1,5 +1,4 @@
 function x = bisection_solver1(fun,x_left,x_right,dxtol,ftol,max_iter)
-    x = [];
     for i = 1:max_iter
         f_left = fun(x_left);
         f_right = fun(x_right);
@@ -8,28 +7,26 @@ function x = bisection_solver1(fun,x_left,x_right,dxtol,ftol,max_iter)
         % plot(x_c,f_c,".","MarkerSize",10, "Color",'r');
     
         if (sign(f_c) == sign(f_right))
-            x = [x,x_right];
-
             x_right = x_c;
-
         else 
-            
-            x = [x,x_left];
             x_left = x_c;
         end
 
         if (abs(x_right)-abs(x_left)) <= dxtol
             fprintf('dxtol\n');
+            x = x_c;
             return
         end
         
         if abs(fun(x_c))<ftol
             fprintf('ftol\n');
+            x=x_c;
             return
         end
 
     hold on
     end
+    x = x_c
     p = log((e-1)/e)/log(e/(e-1));
     fprintf('max_iter\n')
 end
